a15e58d94fc248be1a9c917e0f9d182efd4a7ac2
John Watterson <john@breakthrewbrew.com>
2020-08-13 14:36:29 -0600

    Connect our component AddToDoForm to redux store and actions

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />,
-  </Provider>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
